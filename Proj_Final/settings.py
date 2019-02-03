be2dcb48273b96de18c111ab9586e0aecbc9894f
JC3D <jesteves.undefined@gmail.com>
2019-02-03 19:58:19 -0200

    Alteração de versão python

--- Proj_Final/settings.py
+++ Proj_Final/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'jcestacionamento.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'jcestacionamento.herokuapp.com', 'git.heroku.com/jcestacionamento.git']
 
 
 # Application definition
